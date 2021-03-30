0b3a1154890cfeb6066c362c618f03c5c4cb4e66
ryan <ryan@moltenimmersiveart.com>
2021-03-30 18:29:11 +1030

    v 0.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CSV Output Chataigne Module
- Output values into a CSV
+Output values into an external CSV file.
+
+## How to use
+Create a csv file (this can be blank). Then, in the module's parameters, browse and select this file.
+
+Adding a value adds is like adding a cell to the csv. Adding multiple values builds up a row in the csv.
+
+Saving the row to the file starts a new row.
--- csv.js
+++ csv.js
@@ -1,48 +1,4 @@
-var buffer;
-var csv;
-
-
-function init()
-{
-	//myFloatParam.set(5); //The .set() function set the parameter to this value.
-	//myColorParam.set([1,.5,1,1]);	//for a color parameter, you need to pass an array with 3 (RGB) or 4 (RGBA) values.
-	//myP2DParam.set([1.5,-5]); // for a Point2D parameter, you need to pass 2 values (XY)
-	//myP3DParam.set([1.5,2,-3]); // for a Point3D parameter, you need to pass 3 values (XYZ)
-
-	//local.parameters.csvFile.setAttribute ("directoryMode",true);
-}
-
-/*
- This function will be called each time a parameter of your script has changed
-*/
-function scriptParameterChanged(param)
-{
-	//You can use the script.log() function to show an information inside the logger panel. To be able to actuallt see it in the logger panel, you will have to turn on "Log" on this script.
-	script.log("Parameter changed : "+param.name); //All parameters have "name" property
-	if(param.is(myTrigger)) script.log("Trigger !"); //You can check if two variables are the reference to the same parameter or object with the method .is()
-	else if(param.is(myEnumParam)) script.log("Key = "+param.getKey()+", data = "+param.get()); //The enum parameter has a special function getKey() to get the key associated to the option. .get() will give you the data associated
-	else script.log("Value is "+param.get()); //All parameters have a get() method that will return their value
-}
-
-/*
- This function, if you declare it, will launch a timer at 50hz, calling this method on each tick
-*/
-/*
-function update(deltaTime)
-{
-	script.log("Update : "+util.getTime()+", delta = "+deltaTime); //deltaTime is the time between now and last update() call, util.getTime() will give you a timestamp relative to either the launch time of the software, or the start of the computer.
-}
-*/
-
-
-
-/* ********** MODULE SPECIFIC SCRIPTING **********************
-
-	The "local" variable refers to the object containing the scripts. In this case, the local variable refers to the module.
-	It means that you can access any control inside  this module by accessing it through its address.
-	For instance, if the module has a float value named "Density", you can access it via local.values.density
-	Then you can retrieve its value using local.values.density.get() and change its value using local.values.density.set()
-*/
+var buffer = [];
 
 /*
  This function will be called each time a parameter of this module has changed, meaning a parameter or trigger inside the "Parameters" panel of this module
@@ -63,47 +19,47 @@ function moduleParameterChanged(param)
 	}
 }
 
-/*
- This function will be called each time a value of this module has changed, meaning a parameter or trigger inside the "Values" panel of this module
- This function only exists because the script is in a module
-*/
-function moduleValueChanged(value)
-{
-	if(value.isParameter())
-	{
-		script.log("Module value changed : "+value.name+" > "+value.get());	
-	}else 
-	{
-		script.log("Module value triggered : "+value.name);	
-	}
-}
-
 
 //Commands callbacks
 function addInt(i) {
-
-	local.parameters.csvFile.writeFile(i, true);
+	buffer.push(i);	
 }
 
 function addFloat(f) {
-	local.parameters.csvFile.writeFile(f, true);
+	buffer.push(f);	
 }
 
 function addString(s) {
-	local.parameters.csvFile.writeFile(s, false);
+	buffer.push(s);	
 }
 
 function addBool(b) {
-	local.parameters.csvFile.writeFile(b, false);
+	buffer.push(b);	
 }
 
-function addColor(c) {
-	local.parameters.csvFile.writeFile(c, false);
+function addTimeStamp() {
+	var t = util.getTimestamp();
+	buffer.push(t);
 }
 
-function addRow() {
-}
+function saveToFile() {
+	if(buffer.length > 0) {
 
-function saveCSV() {
+		var fileBuffer = local.parameters.csvFile.readFile(false);
+		var stringBuffer = "";
 
+		for(var i = 0; i < buffer.length; i++) {
+			if(i > 0) {
+				stringBuffer += ", ";
+			}
+			stringBuffer += buffer[i];
+		}
+
+		script.log("writing…");
+		script.log(stringBuffer);
+
+		stringBuffer = fileBuffer + stringBuffer + "\n";
+		local.parameters.csvFile.writeFile(stringBuffer, true);
+		buffer = [];
+	}
 }
